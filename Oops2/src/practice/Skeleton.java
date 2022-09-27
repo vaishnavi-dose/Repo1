1534c218d917cb66f4a59a8e5591f3de17e41cd5
vaishnavi-dose <114053692+vaishnavi-dose@users.noreply.github.com>
2022-09-27 13:55:31 +0530

    Commit to "Abstraction and Interface"

--- Oops2/src/practice/Skeleton.java
+++ Oops2/src/practice/Skeleton.java
@@ -1,5 +1,9 @@
 package practice;
 
 public interface Skeleton {
+	
+	void brain();
+	void flesh();
+	void color();
 
 }
--- Oops2/src/practice/Vaishnavi.java
+++ Oops2/src/practice/Vaishnavi.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class Vaishnavi {
+public class Vaishnavi implements Skeleton,IQ {
+
+	@Override
+	public void brain() {
+		// TODO Auto-generated method stub
+		System.out.println("I am Monster");
+	}
+
+	@Override
+	public void flesh() {
+		// TODO Auto-generated method stub
+		System.out.println("I am Healthy");
+	}
+
+	@Override
+	public void color() {
+		// TODO Auto-generated method stub
+		System.out.println("I am Fair");
+	}
 
 }
