0dd409f26f6fe8c775d6e887d62e82754611b35c
vaishnavi-dose <114053692+vaishnavi-dose@users.noreply.github.com>
2022-10-10 18:27:24 +0530

    Commit Revision

--- Oops2/src/practice/Animal.java
+++ Oops2/src/practice/Animal.java
@@ -1,5 +1,24 @@
 package practice;
 
-public abstract class Animal {
+public class Animal implements Lion, Tiger {
 
+	@Override
+	public String sound() {
+		// TODO Auto-generated method stub
+		return "roar";
+	}
+
+	@Override
+	public String color() {
+		// TODO Auto-generated method stub
+		return "yellow";
+	}
+
+	@Override
+	public int No_of_Legs() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+	
 }
